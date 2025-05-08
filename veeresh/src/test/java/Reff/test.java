3083c1c92a8bd8fbad5b8fe89a0d41218e9014a1
VEERESH <VEERESH@LAPTOP-H4AIVH69>
2025-05-08 13:05:39 +0530

    We have added cod efor this project

--- veeresh/src/test/java/Reff/test.java
+++ veeresh/src/test/java/Reff/test.java
@@ -1,10 +1,27 @@
 package Reff;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 @Test
+	 public void m1()
+	 {
+	
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.saucedemo.com/");
+System.out.println(driver.getTitle());
+try
+{
+	Thread.sleep(5000);
+}
+catch(InterruptedException e)
+{
+e.printStackTrace()	;
+}
+driver.close();
 	}
 
 }
